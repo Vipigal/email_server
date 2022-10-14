879586930dd08542ada4e414a55fcf9af5b803ef
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2022-10-14 17:11:25 -0300

    feat: implementacao prioridade na fila

--- src/main.cpp
+++ src/main.cpp
@@ -10,13 +10,20 @@ int main(int argc, char const *argv[]){
 	std::ifstream input("input.txt");
 	std::string buffer;
 
+	User* users;
+
 	while(getline(input,buffer)){
 		
-		std::cout<<buffer<<std::endl;
+		
+		if(buffer.find("CADASTRA")!=std::string::npos){
+			//A posicao [9] da string sempre sera o ID do usuario
+			
+			std::cout<<buffer[9]<<std::endl;
+		}
 
 	}
 
-	User* users;
+	
 
 
 
